58425b28776f33a274e3ee040ac88fda8a8fbf80
Bogdan Patratanu <bogdan.patratanu@gmail.com>
2026-01-24 11:41:50 +0200

    variables

--- src/dataProvider.tsx
+++ src/dataProvider.tsx
@@ -1,3 +1,3 @@
 import crudProvider from "ra-data-nestjsx-crud";
 
-export const dataProvider = crudProvider(import.meta.env.BACKEND_URL);
+export const dataProvider = crudProvider(import.meta.env.VITE_BACKEND_URL);
